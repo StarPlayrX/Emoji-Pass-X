9771fe4fa89313fe435f89aa47f14489f0fdc4e2
Todd Bruss <admin@starplayrx.com>
2021-07-25 00:46:16 -0400

    remove space

--- Emoji Pass X/Delegates/AppDelegate.swift
+++ Emoji Pass X/Delegates/AppDelegate.swift
@@ -21,7 +21,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
         // Autolayout will complain less
         UserDefaults.standard.set(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
         return true
