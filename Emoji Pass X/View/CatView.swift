4c336e07bd11b7cc4a6ea83ebcdbf2c10d4a9df3
Todd Bruss <admin@starplayrx.com>
2021-07-25 00:59:58 -0400

    ok

--- Emoji Pass X/View/CatView.swift
+++ Emoji Pass X/View/CatView.swift
@@ -50,8 +50,32 @@ struct CatView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Text("Welcome\n\nApple Natives")
-            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-            .multilineTextAlignment(.center)
+        /*Text("Welcome\n\nApple Natives")
+         .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+         .multilineTextAlignment(.center)*/
+        Group {
+            VStack {
+                Text("Emoji Pass X").font(.largeTitle).minimumScaleFactor(0.75).padding(.top, 50)
+                
+                HStack {
+                    Image("Emoji Pass X_logo4")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 230.0, height: 230)
+                        .background(Color.clear)
+                }
+                .overlay(
+                    RoundedRectangle(cornerRadius: 48)
+                        .stroke(Color.gray, lineWidth: 2)
+                )
+                
+                Text("© 2021 Todd Bruss").font(.callout).minimumScaleFactor(0.75).padding(.top, 10)
+                    .padding(.bottom, 25)
+                Button("Continue") {}
+            }
+        }
+        
     }
+    
+    
 }
